3e04fc7762059fdda0ec0beb16172dbc8fd83b82
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-23 23:29:48 +0200

    feat: Přidán 'useCallback' a stav 'isMobileView' do komponenty Header - Přidán 'useCallback' pro optimalizaci funkce 'handleMobileNavToggle' - Přidán stav 'isMobileView' pro sledování velikosti obrazovky a podmíněné zobrazování 'hamburger' tlačítka

--- components/header.tsx
+++ components/header.tsx
@@ -8,27 +8,36 @@ import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { toggleMobileNav } from '@/store/slices/ui-slice';
 import { defaultTransition } from '@/utils/styles';
 import { throttle } from 'lodash';
-
-const throttledToggleMobileNav = throttle((dispatch) => {
-  dispatch(toggleMobileNav());
-}, 200);
+import { useCallback, useEffect } from 'react';
 
 export default function Header() {
   const dispatch = useAppDispatch();
   const isMobileNavOpen = useAppSelector((state) => state.ui.isMobileNavOpen);
+  const isMobileView = useAppSelector((state) => state.ui.isMobileView);
 
-  const handleMobileNavToggle = () => {
-    throttledToggleMobileNav(dispatch);
-  };
+  const handleMobileNavToggle = useCallback(
+    throttle(() => {
+      dispatch(toggleMobileNav());
+    }, 200),
+    [dispatch]
+  );
+
+  useEffect(() => {
+    return () => {
+      handleMobileNavToggle.cancel();
+    };
+  }, [handleMobileNavToggle]);
 
   return (
     <header className="fixed inset-0 h-fit w-full bg-backgroundSecondary">
       <div className={clsx(isMobileNavOpen ? 'h-screen' : 'h-14', defaultTransition, 'container overflow-hidden')}>
         <div className="flex h-14 items-center justify-between">
           <Logo className="h-6" />
-          <button onClick={handleMobileNavToggle} className="h-7 w-7">
-            {isMobileNavOpen ? <FiX className="h-full w-full" /> : <FiMenu className="h-full w-full" />}
-          </button>
+          {isMobileView && (
+            <button onClick={handleMobileNavToggle} className="h-7 w-7" aria-expanded={isMobileNavOpen}>
+              {isMobileNavOpen ? <FiX className="h-full w-full" /> : <FiMenu className="h-full w-full" />}
+            </button>
+          )}
         </div>
         <nav
           className={clsx(
