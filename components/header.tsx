4af839668ca86294333a950abe9ac7c311c76dd0
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-09-23 21:35:27 +0200

    refactor: Optimalizace 'toggle' funkce mobilní navigace s použitím throttlingu z 'lodash'

--- components/header.tsx
+++ components/header.tsx
@@ -7,19 +7,26 @@ import { FiX } from 'react-icons/fi';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { toggleMobileNav } from '@/store/slices/ui-slice';
 import { defaultTransition } from '@/utils/styles';
+import { throttle } from 'lodash';
+
+const throttledToggleMobileNav = throttle((dispatch) => {
+  dispatch(toggleMobileNav());
+}, 200);
 
 export default function Header() {
   const dispatch = useAppDispatch();
   const isMobileNavOpen = useAppSelector((state) => state.ui.isMobileNavOpen);
 
-  const handleMenuButtonClick = () => dispatch(toggleMobileNav());
+  const handleMobileNavToggle = () => {
+    throttledToggleMobileNav(dispatch);
+  };
 
   return (
     <header className="fixed inset-0 h-fit w-full bg-backgroundSecondary">
       <div className={clsx(isMobileNavOpen ? 'h-screen' : 'h-14', defaultTransition, 'container overflow-hidden')}>
         <div className="flex h-14 items-center justify-between">
           <Logo className="h-6" />
-          <button onClick={handleMenuButtonClick} className="h-7 w-7">
+          <button onClick={handleMobileNavToggle} className="h-7 w-7">
             {isMobileNavOpen ? <FiX className="h-full w-full" /> : <FiMenu className="h-full w-full" />}
           </button>
         </div>
